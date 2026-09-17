c8eb474a2aa88d5d51fe6b321452b4864003f4fb
ananyaarora <ananya5254@gmail.com>
2026-09-17 23:14:59 +0530

    added releas workflow

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.Properties
+import java.io.FileInputStream
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.compose)
@@ -5,6 +7,12 @@ plugins {
     alias(libs.plugins.ksp)
     id("jacoco")
 
+}
+val keystorePropertiesFile = rootProject.file("keystore.properties")
+val keystoreProperties = Properties()
+
+if (keystorePropertiesFile.exists()) {
+    keystoreProperties.load(FileInputStream(keystorePropertiesFile))
 }
 android {
     namespace = "com.example.pokemonapp"
@@ -26,6 +34,13 @@ android {
         release {
             optimization {
                 enable = false
+            }
+
+            signingConfig = signingConfigs.create("release") {
+                storeFile = rootProject.file(keystoreProperties["storeFile"] as String)
+                storePassword = keystoreProperties["storePassword"] as String
+                keyAlias = keystoreProperties["keyAlias"] as String
+                keyPassword = keystoreProperties["keyPassword"] as String
             }
         }
     }
